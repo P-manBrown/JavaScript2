bb6c88c2221d756e676fafeda29fc60ccb3b0264
P-manBrowm <tachikawa.yuya@outlook.jp>
2021-12-05 16:04:37 +0900

    [Finish] 2-4-sengen

---
+++ sample.js
@@ -0,0 +1,64 @@
+// var nickname = "taro"
+// console.log(nickname)
+// var nickname = "ichiro"
+// console.log(nickname)
+
+// let nickname = "taro"
+// console.log(nickname)
+// let nickname = "ichiro"
+// console.log(nickname)
+
+// const nickname = "taro"
+// console.log(nickname)
+// const nickname = "ichiro"
+// console.log(nickname)
+
+// var nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// let nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// const nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// var str = "webcamp"
+
+// function foo() {
+//   console.log(str)
+//   var y = "hello"
+// }
+
+// foo()
+// console.log(y)
+  
+// function foo(){
+//   let x = "webcamp"
+//   {
+//     let y = "hello webcamp"
+//   }
+//   console.log(x)
+//   console.log(y)
+// }
+// foo()
+
+// for (let i = 0; i < 10; i++){
+//   console.log(i)
+// }
+// console.log(i)
+
+var str = "webcamp"
+
+function foo() {
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
+}
+
+foo()
